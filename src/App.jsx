a352fae7c2fae9c5607139256068b511f0db1a0f
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-21 23:32:13 +0530

    custom theme added

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,39 @@ import { Gallery } from "./components/Gallery";
 
 //MUI components
 import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const App = () => {
+    const theme = createTheme({
+        components: {
+            MuiTypography: {
+                variants: [
+                    {
+                        props: {
+                            variant: "body2",
+                        },
+                        style: {
+                            fontSize: 14,
+                        },
+                    },
+                    {
+                        props: {
+                            variant: "body3",
+                        },
+                        style: {
+                            fontSize: 12,
+                        },
+                    },
+                ],
+            },
+        },
+    });
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <Container>
                 <Gallery />
             </Container>
-        </>
+        </ThemeProvider>
     );
 };
 
